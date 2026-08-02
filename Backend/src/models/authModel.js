e9e6f5654511640615f89bf58e18d663667fdc3c
Manita-tech-dot <manitaadangal@gmail.com>
2026-08-02 21:30:21 +0545

    add authModel for register/login DB logic

---
+++ Backend/src/models/authModel.js
@@ -0,0 +1,15 @@
+import User from "../../data/user.js";
+import bcrypt from "bcryptjs";
+
+export async function register(userDetails) {
+  const existing = await User.findOne({ email: userDetails.email });
+  if (existing) throw new Error("Email is already registered");
+  return await User.create(userDetails);
+}
+
+export async function login({ email, password }) {
+  const user = await User.findOne({ email });
+  if (!user) throw new Error("Invalid email or password");
+  const isValid = await bcrypt.compare(password, user.password);
+  return isValid ? user : null;
+}
